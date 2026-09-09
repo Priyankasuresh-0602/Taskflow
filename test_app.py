0174aaa8b8d73337df4729ecb15912ba6160ac22
Priyanka <psuresh@student.gitam.edu>
2026-09-09 20:52:01 +0530

    Fix broken test

--- test_app.py
+++ test_app.py
@@ -14,7 +14,7 @@ def client():
 def test_health(client):
     r = client.get("/health")
     assert r.status_code == 200
-    assert r.get_json()["status"] == "failed"
+    assert r.get_json()["status"] == "ok"
 
 
 def test_create_and_list(client):
